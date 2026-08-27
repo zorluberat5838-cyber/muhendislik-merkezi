c909999850e74bcd7be8838c78905546418ce099
Berat Buğra Zorlu <berat@example.com>
2026-08-27 16:43:28 +0300

    Hosting port ayari

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const Database = require("better-sqlite3");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 const db = new Database("./data/site.db");
 
 db.exec(`
